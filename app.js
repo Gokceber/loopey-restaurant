3b48ccdee112e564802202a4b5b7fffd26915eb6
“Gokce <gokceberndt@gmail.com>
2023-05-31 14:31:40 +0200

    handlebars layout, views, partials

--- app.js
+++ app.js
@@ -1,25 +1,80 @@
 const express = require("express");
+const hbs = require("hbs");//requiring the handlebars package
 
 const app = express();
 
-//app.get(path, code);
-//app.get (path, (req,res, next) => {});
+app.use(express.static('public')); // Make everything inside of public/ available
 
-// GET HOME
-app.get("/", function (req, res, next){
-    console.log("we have received a request");
-    res.sendFile(__dirname + "/views/home-page.html");
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials"); //tell HBS which directory we use for partials
+
+// app.get(path, code);
+// app.get(path, (req, res, next) => {});
+
+
+
+
+
+
+// GET /
+app.get("/", (req, res, next) => {
+   res.render("home-page")
 });
 
-//GET CONTACT
-app.get("/contact", (req,res,next)=>{
-    res.sendFile(__dirname + "/views/contact-page.html")
-})
 
-app.use(express.static('public')); //Make everything in public folder available
-
+// GET /contact
+app.get("/contact", (req, res, next) => {
+    res.render("contact-page")
+});
 
 
+// GET /pizzas/margarita
+app.get("/pizzas/margarita", (req, res, send) => {
+
+    const pizzaDetails = {
+        title: 'Pizza Margarita',
+        price: 12,
+        recommendedDrink: 'beer',
+        imageFile: 'pizza-margarita.jpg',
+        ingredients: ['mozzarella', 'tomato sauce', 'basilicum'],
+    };
+
+    res.render("product", pizzaDetails);
+});
 
 
-app.listen(3000); //port number(localhost:3000/about)--then message shows up(line6)
+
+// GET /pizzas/veggie
+app.get("/pizzas/veggie", (req, res, send) => {
+
+    const pizzaDetails = {
+        title: 'Veggie Pizza',
+        price: 15,
+        recommendedDrink: 'power smoothie',
+        imageFile: 'pizza-veggie.jpg',
+        ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+    };
+
+    res.render("product", pizzaDetails)
+});
+
+
+
+
+// GET /pizzas/seafood
+app.get("/pizzas/seafood", (req, res, send) => {
+
+    const pizzaDetails = {
+        title: 'Seafood Pizza',
+        recommendedDrink: 'white wine',
+        imageFile: 'pizza-seafood.jpg',
+        ingredients: ['tomato sauce', 'garlic', 'prawn'],
+    };
+
+    res.render("product", pizzaDetails);
+});
+
+
+app.listen(3000, () => { console.log("server listening on port 3000...")});
