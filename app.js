b116853ada6bdb8966d95b30bfe2f84493b498a3
“Gokce <gokceberndt@gmail.com>
2023-06-02 15:23:21 +0200

    add price filter and input fields

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const hbs = require("hbs"); //requiring the handlebars package
 const Pizza = require("./models/Pizza.model");
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 
-
-
 mongoose
-  .connect('mongodb://127.0.0.1/loopeyRestaurant')
-  .then(x => {
+  .connect("mongodb://127.0.0.1/loopeyRestaurant")
+  .then((x) => {
     console.log(`Connected! Database name: "${x.connections[0].name}"`);
   })
-  .catch(e => ("error connecting to DB", e))
+  .catch((e) => ("error connecting to DB", e));
 
 app.use(express.static("public")); // Make everything inside of public/ available
 
@@ -66,10 +64,10 @@ app.get("/pizzas/seafood", (req, res, send) => {
 
 //// ROUTE PARAMS ////
 // :aNameYouChoose(syntax)
-app.get("/drinks/:drinkName", (req,res,next)=>{
-    console.log(req.params);
-    res.send(`display info about....${req.params.drinkName}`)
-})
+app.get("/drinks/:drinkName", (req, res, next) => {
+  console.log(req.params);
+  res.send(`display info about....${req.params.drinkName}`);
+});
 
 //lines between 37-64 were creating specific routes for each pizza
 //but it is not sustainable when you have 200 diff. pizzas
@@ -77,31 +75,38 @@ app.get("/drinks/:drinkName", (req,res,next)=>{
 //that can be reused for each different item
 //so, below I create a generic route with param for this purpose
 
+app.get("/pizzas/:pizzaName", (req, res, next) => {
+  //console.log(req.params)
+  //res.send(`displaying info about...${req.params.pizzaName}`)
+  //res.render("product", ${req.params.pizzaName})
 
-app.get("/pizzas/:pizzaName", (req,res,next)=> {
-    //console.log(req.params)
-    //res.send(`displaying info about...${req.params.pizzaName}`)
-    //res.render("product", ${req.params.pizzaName})
-
-    Pizza.findOne({title:req.params.pizzaName })
-        .then((pizzaFromDB)=>{
-        res.render("product", pizzaFromDB);  
-        })
-        .catch(e=>console.log("error getting pizza from DB", e));
-
-})
-
-app.get("/pizzas", (req,res,next)=>{
-    
-    Pizza.find()
-    .then((pizzas)=>{
-    
-        const data = {
-            pizzasArr: pizzas
-        }
-        res.render("product-list", data)
+  Pizza.findOne({ title: req.params.pizzaName })
+    .then((pizzaFromDB) => {
+      res.render("product", pizzaFromDB);
     })
-    .catch(e=>console.log("there was an error"))
+    .catch((e) => console.log("error getting pizza from DB", e));
+});
+
+app.get("/pizzas", (req, res, next) => {
+  //console.log(req.query) req.query is an object
+  //console.log(req.query.maxPrice) we will receive a string
+  console.log(req.query.maxPrice);
+
+  const maximumPrice = Number(req.query.maxPrice); //convert it to a number
+  console.log(maximumPrice)
+
+  let filter = {};
+  if (maximumPrice) {
+    filter = { price: { $lte: maximumPrice } };
+  }
+  Pizza.find({ price: { $lte: maximumPrice } })
+    .then((pizzas) => {
+      const data = {
+        pizzasArr: pizzas,
+      };
+      res.render("product-list", data);
+    })
+    .catch((e) => console.log("there was an error"));
 });
 
 app.listen(3000, () => {
